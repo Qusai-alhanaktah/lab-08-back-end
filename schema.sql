1326197204b3e86326dee97fc6d6867fe829eb8c
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2019-12-25 17:12:23 +0200

    still

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE location (
     formatted_query VARCHAR(255),
     latitude VARCHAR(255),
     longitude VARCHAR(255),
-    search_query VARCHAR(255),
+    search_query VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS weather;
@@ -13,7 +13,7 @@ DROP TABLE IF EXISTS weather;
 CREATE TABLE weather (
     id SERIAL PRIMARY KEY,
     time VARCHAR(255),
-    forecast VARCHAR(255),
+    forecast VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS events;
@@ -23,5 +23,5 @@ CREATE TABLE events (
     link VARCHAR(255),
     name VARCHAR(255),
     event_date VARCHAR(255),
-    summary VARCHAR(255),
+    summary VARCHAR(255)
 );
