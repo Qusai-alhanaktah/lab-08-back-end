5f351419aeea04bab70638aa48115c09c0c50f50
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2019-12-27 16:52:57 +0200

    finished

--- schema.sql
+++ schema.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS location;
 
 CREATE TABLE location (
     id SERIAL PRIMARY KEY,
+    search_query VARCHAR(255),
     formatted_query VARCHAR(255),
-    latitude NUMERIC(255),
-    longitude NUMERIC(255),
-    search_query VARCHAR(255)
+    latitude NUMERIC(10, 7),
+    longitude NUMERIC(10, 7)
 );
 
 DROP TABLE IF EXISTS weather;
--- server.js
+++ server.js
@@ -15,10 +15,6 @@ const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY;
 const EVENTFUL_API_KEY = process.env.EVENTFUL_API_KEY;
 
-const client = new pg.Client(process.env.DATABASE_URL);
-client.on('error', error=>{throw error;})
-
-
 
 server.get('/', (request, response) => {
     response.status(200).send('Okay its found');
@@ -37,7 +33,7 @@ server.get('/', (request, response) => {
 // server.get('/add', (request,response)=>{
 //     let first = request.query[`first`];
 //     let last = request.query[`last`];
-    
+
 //     let sql = `INSERT INTO people(first_name, last_name) VALUES ($1, $2) RETURNING*`
 //     let queryData = [first, last];
 //     client.query(sql,queryData)
@@ -50,34 +46,45 @@ server.get('/', (request, response) => {
 
 server.get('/location', locationRndering);
 
-function Location(city, locationData) {
-    this.formatted_query = locationData[0].display_name;
-    this.latitude = locationData[0].lat;
-    this.longitude = locationData[0].lon;
-    this.search_query = city;
+function Location(locationData) {
+    this.formatted_query = locationData.display_name;
+    this.latitude = locationData.lat;
+    this.longitude = locationData.lon;
 }
 
 function locationRndering(request, response) {
-    let city = request.query['city'];
-    let sql = `SELECT * FROM location`;
-    let newSql = `INSERT INTO location(city) VALUES ($1) RETURNING*`
-    let queryData = [city];
-    if (!queryData.include(city)) client.query(sql,queryData);
-    else client.query(sql,queryData);
-
+    let city = request.query.city;
+    
     getLocationData(city)
-        .then((data) => {
-            response.status(200).send(data);
-        });
+        .then(data => render(data, response))
+        .catch((error) => errorHandler(error, request, response));
 }
+ 
 function getLocationData(city) {
-    const locationUrl = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json&limit=1`;
-    return superagent.get(locationUrl)
-        .then((data) => {
-            // console.log(data.body)
-            const location = new Location(city, data.body);
-            return location;
-        });
+    // const locationUrl = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json&limit=1`;
+    // return superagent.get(locationUrl)
+    //     .then((data) => {
+    //         const location = new Location(city, data.body);
+    //         return location;
+        // });
+         let sql = `SELECT * FROM location WHERE search_query = $1`;
+    let queryData = [city];
+    return client.query(sql, queryData)
+        .then(result => {
+            if (result.rowCount) {return result.rows[0];}
+             else {
+                const locationUrl = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json&limit=1`;
+              return superagent.get(locationUrl)
+                .then(data=> cacheLocation(city, data.body));
+                }
+            });
+}
+function cacheLocation(city, data){
+            const location = new Location(data[0]);
+            let newSql = `INSERT INTO location(search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) RETURNING*`
+            let values = [city, location.display_query, location.latitude, location.longitude];
+            return client.query(newSql, values)
+            .then(result=> result.rows[0]);
 }
 // //////////////////////
 server.get('/weather', weatherrenderring);
@@ -86,34 +93,34 @@ function Weather(day) {
     this.time = new Date(day.time * 1000).toDateString()
     this.forecast = day.summary;
 }
-function weatherrenderring(request,response){
+function weatherrenderring(request, response) {
     let lat = request.query['latitude'];
     let lng = request.query['longitude'];
-    getWeatherData(lat,lng)
-    .then((data) =>{
-        response.status(200).send(data);
-    });
-    }
-function getWeatherData(lat,lng){
+    getWeatherData(lat, lng)
+        .then((data) => {
+            response.status(200).send(data);
+        });
+}
+function getWeatherData(lat, lng) {
     const weatherUrl = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${lat},${lng}`;
     return superagent.get(weatherUrl)
-    .then((weatherData) =>{
-        let weather = weatherData.body.daily.data.map((day) => new Weather(day));
-        return weather;
-    });
+        .then((weatherData) => {
+            let weather = weatherData.body.daily.data.map((day) => new Weather(day));
+            return weather;
+        });
 }
 // ///////////////////////////
 
 //     {
-    //       "link": "http://seattle.eventful.com/events/seattle-code-101-explore-software-development-/E0-001-126675997-3?utm_source=apis&utm_medium=apim&utm_campaign=apic",
-    //       "name": "Seattle Code 101: Explore Software Development",
-    //       "event_date": "Sat Dec 7 2019",
-    //       "summary": "Thinking about a new career in software development? Start here! In this one-day workshop, you&#39;ll get a taste of a day in the life of a software developer. Code 101 helps you learn what it’s like to be a software developer through a day-long immersive course for beginners that focuses on front-end web development technologies. "
-    //     },
-    server.get('/events', eventfulRndering);
+//       "link": "http://seattle.eventful.com/events/seattle-code-101-explore-software-development-/E0-001-126675997-3?utm_source=apis&utm_medium=apim&utm_campaign=apic",
+//       "name": "Seattle Code 101: Explore Software Development",
+//       "event_date": "Sat Dec 7 2019",
+//       "summary": "Thinking about a new career in software development? Start here! In this one-day workshop, you&#39;ll get a taste of a day in the life of a software developer. Code 101 helps you learn what it’s like to be a software developer through a day-long immersive course for beginners that focuses on front-end web development technologies. "
+//     },
+server.get('/events', eventfulRndering);
 
 function Eventful(eventData) {
-    this.link  = eventData[0].url;                                                                                                                                               
+    this.link = eventData[0].url;
     this.name = eventData[0].title;
     this.event_date = eventData[0].start_time;
     this.summary = eventData[0].description;
@@ -131,10 +138,10 @@ function getEventfulData(city) {
     const eventfulUrl = `http://api.eventful.com/json/events/search?app_key=${EVENTFUL_API_KEY}&location=${city}`;
     // console.log(eventfulUrl)
     return superagent.get(eventfulUrl)
-    .then((eventfulData) => {
-        let jsonData = JSON.parse(eventfulData.text).events.event;
-        console.log(jsonData);
-        const eventful = jsonData.map((day) => new Eventful(jsonData));
+        .then((eventfulData) => {
+            let jsonData = JSON.parse(eventfulData.text).events.event;
+            console.log(jsonData);
+            const eventful = jsonData.map((day) => new Eventful(jsonData));
 
             return eventful;
         });
@@ -147,11 +154,21 @@ server.use('*', (request, response) => {
 server.use((error, request, response) => {
     response.status(500).send("Sorry, something went wrong");
 });
+function render(data, response){
+    response.status(200).json(data)
+}
+function notFoundHandler(data, response){
+    response.status(404).send("NO;")
+}
+function errorHandler(error, request, response){
+    response.status(500).send(error)
+} 
+function startServer(){
+    server.listen(PORT, () => console.log('its work'));
 
+}
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', err => console.error(err));
 client.connect()
-.then (()=>{
-    server.listen(PORT, ()=>console.log('its work'));
-})
-.catch(err => {
-    throw `Error happend ${err}`;
-})
+    .then(startServer)
+    .catch(err => console.error(err));
